f1f6613d78d3a60dd2d9c378e0c2fde850de362c
Kostya Cholak <kostya.cholak@gmail.com>
2024-06-12 19:53:54 +0200

    feat: add requiremets.txt

--- src/audio_generation.py
+++ src/audio_generation.py
@@ -35,11 +35,11 @@ def pad_audio_file(file_path: str, duration: int):
 
 
 def generate_audio(text: str, save_as: str) -> VoiceClip:
-    client = ElevenLabs(
-        api_key=os.getenv("ELEVEN_API_KEY"),
-    )
-    output = client.generate(text=text)
-    save(output, save_as)
+    # client = ElevenLabs(
+    #     api_key=os.getenv("ELEVEN_API_KEY"),
+    # )
+    # output = client.generate(text=text)
+    # save(output, save_as)
     
     dur = get_audio_length(save_as)
     pad_audio_file(save_as, dur)
@@ -49,5 +49,3 @@ def generate_audio(text: str, save_as: str) -> VoiceClip:
         file_path=save_as,
         duration=dur,
     )
-    
-    
